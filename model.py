4fb2bdd387c757b0a9ab0b7d933712753f9454e3
hxf1228 <xufenghuang1228@gmail.com>
2021-02-03 02:19:18 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,10 @@
+"""
+
+Created on: 2/3/2021 2:18 AM 
+
+@File: model.py
+@Author: Xufeng Huang (xufenghuang1228@gmail.com & xfhuang@umich.edu)
+@Copy Right: Licensed under the MIT License. 
+
+"""
+
